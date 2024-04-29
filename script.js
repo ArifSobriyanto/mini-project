4bf64426f00d116ad64dffa7063264a5229bdda6
ArifSobriyanto <160447436+ArifSobriyanto@users.noreply.github.com>
2024-04-29 12:17:23 +0700

    0.3

--- script.js
+++ script.js
@@ -5,3 +5,10 @@ document.addEventListener("DOMContentLoaded", function() {
         if (this.readyState == 4 && this.status == 200) {
             // Parse JSON yang diterima
             var data = JSON.parse(this.responseText);
+
+            // Tampilkan informasi toko
+            document.getElementById("nama-toko").textContent = data.toko;
+            document.getElementById("pengarang-toko").textContent = data.pengarang;
+            document.getElementById("alamat-toko").textContent = data.alamat;
+
+   
